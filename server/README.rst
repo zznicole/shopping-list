6d9a08c8645a8f7e362f1c5162627bed1f6c8faa
Tomas Andersson <tomas@clear-objects.com>
2020-09-28 20:48:54 +0200

    Added server side code for serving the react build and provide the api for managing shopping lists

---
+++ server/README.rst
@@ -0,0 +1,8 @@
+My shopping list
+================
+
+Node modules required:
+
+* express.js: npm install express
+* config: npm install config
+* moment: npm install moment
---
+++ server/script/users.js
@@ -0,0 +1,33 @@
+const dboo = require('dboo.node');
+const config = require('config');
+const user = require('../src/user.js');
+
+dbConfig = config.get('dbConfig');
+
+const odb = new dboo.ODB();
+odb.connect(dbConfig.host, dbConfig.port, dbConfig.dbName, dbConfig.webUserName, dbConfig.webUserPwd);
+
+function makeUserAdmin(userId)
+{
+  let users = [];
+  odb.query(users, "select<User>(eq(userId,\"" + dboo.escape_string(userId) + "\"))")
+  if (users.length == 1) {
+    let usr = users[0];
+    console.log(usr);
+    if (!usr.permissions.includes(user.admin)) {
+      usr.permissions.push(user.admin);
+      console.log(usr);
+      odb.commit(usr);
+      console.log("User %s updated, was made an administrator.", userId);
+    } else {
+      console.log("User %s not updated, already an administrator.", userId);
+    }
+  } else {
+    console.log("No user named '%s' exist.", userId);
+  }
+}
+
+
+if ((process.argv.length == 4) && process.argv[2] == "makeAdmin") {
+  makeUserAdmin (process.argv[3]);
+}
